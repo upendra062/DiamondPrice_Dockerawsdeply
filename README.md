dedbf380eb8f098fe1d442ae1bb4dd413c387ebb
upendra <bhadauriaupendra062@gmail.com>
2023-04-19 17:14:37 +0530

    Triggering the apprunner

--- README.md
+++ README.md
@@ -197,6 +197,6 @@ AWS_REGION = us-east-1
 
 AWS_ECR_LOGIN_URI = demo>> 566373416292.dkr.ecr.ap-south-1.amazonaws.com
 
-ECR_REPOSITORY_NAME = simple-app
+ECR_REPOSITORY_NAME = your-app
 
 ##### ec2 and ecr
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask , request, render_template, jsonify 
 from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
 
-
 application = Flask(__name__)
 app = application 
 
